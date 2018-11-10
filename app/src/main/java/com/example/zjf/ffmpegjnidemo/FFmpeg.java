9682cad0706a59ef4fff4bebf3207f93e29e83b1
Xiaoben336 <614306572@qq.com>
2018-11-10 22:54:45 +0800

    Initial commit

---
+++ app/src/main/java/com/example/zjf/ffmpegjnidemo/FFmpeg.java
@@ -0,0 +1,17 @@
+package com.example.zjf.ffmpegjnidemo;
+
+public class FFmpeg {
+    static {
+        System.loadLibrary("avutil-55");
+        System.loadLibrary("avcodec-57");
+        System.loadLibrary("avformat-57");
+        System.loadLibrary("avdevice-57");
+        System.loadLibrary("swresample-2");
+        System.loadLibrary("swscale-4");
+        System.loadLibrary("postproc-54");
+        System.loadLibrary("avfilter-6");
+        System.loadLibrary("ffmpeg");
+    }
+
+    public static native void run();
+}
